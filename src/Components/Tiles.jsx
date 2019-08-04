d69c96dc32963c37688c13194916a0ef0b5fc9b0
chaitral <chaitralthakur74@yahoo.in>
2019-08-04 14:10:40 +0530

    first commit

---
+++ src/Components/Tiles.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+
+ return (
+ <div>
+      <h1 className="title-container__title">Weather_app </h1>
+     <p className="title-container__subtitle"> Helps you find weather conditions ...  </p>
+ </div>
+ )
+}
+}
+
+export default Titles;
